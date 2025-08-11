c951e3a92f2c886c1a06619e8b99bdfabcfeb7bd
xTaiwanPingLord <52948923+xTaiwanPingLord@users.noreply.github.com>
2025-08-11 13:47:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,29 +10,33 @@ UPDATE_SERVICE_PATH="/etc/systemd/system/update_fix.service"
 
 # Check installation file exist in current working folder
 DUALBOOT_FILE="dualboot_fix.sh"
-UPDATE_FILE="dualboot_fix.sh"
+DUALBOOT_SERVICE_FILE="dualboot_fix.service"
+UPDATE_SERVICE_FILE="dualboot_fix.sh"
 DEFAULT_FOLDER="/home/deck/SD_fix"
 
 # Check in current working directory
 if [ -f "$DUALBOOT_FILE" ]; then
     echo "Found $DUALBOOT_FILE in $(pwd)"
     DUALBOOT_FILE_PATH="$(pwd)/$DUALBOOT_FILE"
-    UPDATE_FILE_PATH="$(pwd)/$UPDATE_FILE"
+    DUALBOOT_SERVICE_FILE_PATH="$(pwd)/$DUALBOOT_SERVICE_FILE"
+    UPDATE_SERVICE_FILE_PATH="$(pwd)/$UPDATE_SERVICE_FILE"
 else
     echo "$DUALBOOT_FILE not found in $(pwd), using default folder"
     DUALBOOT_FILE_PATH="$DEFAULT_FOLDER/$DUALBOOT_FILE"
-    UPDATE_FILE_PATH="$DEFAULT_FOLDER/$UPDATE_FILE"
+    DUALBOOT_SERVICE_FILE_PATH="$DEFAULT_FOLDER/$DUALBOOT_SERVICE_FILE"
+    UPDATE_SERVICE_FILE_PATH="$DEFAULT_FOLDER/$UPDATE_SERVICE_FILE"
 fi
 
 echo "Using file: $DUALBOOT_FILE_PATH"
-echo "Using file: $UPDATE_FILE_PATH"
+echo "Using file: $DUALBOOT_SERVICE_FILE_PATH"
+echo "Using file: $UPDATE_SERVICE_FILE_PATH"
 
 # Copy script to system path
 install -m 755 -o root -g root "$DUALBOOT_FILE_PATH" "$SCRIPT_PATH"
 
 # Copy service file to system path
-install -m 644 -o root -g root "$DUALBOOT_FILE_PATH" "$DUALBOOT_ERVICE_PATH"
-install -m 644 -o root -g root "$UPDATE_FILE_PATH" "$UPDATE_SERVICE_PATH"
+install -m 644 -o root -g root "$DUALBOOT_SERVICE_FILE_PATH" "$DUALBOOT_SERVICE_PATH"
+install -m 644 -o root -g root "$UPDATE_SERVICE_FILE_PATH" "$UPDATE_SERVICE_PATH"
 
 echo "[+] Reloading systemd and enabling service..."
 systemctl daemon-reload
